e5451cacd4c4daad326e9948a86b22e129138702
IT18128246 <It18128246@my.sliit.lk>
2021-09-22 07:44:26 +0530

    google-calendar service added

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const session = require('express-session');
+const nunjucks = require('nunjucks')
+const indexRouter = require('./routes/index.route')
+
+const app = express()
+
+require('dotenv').config()
+
+// nunjucks config
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+// used to parse JSON bodies
+app.use(express.json())
+
+//parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+    session({
+        name: 'sid',
+        saveUninitialized: false,
+        resave: false,
+        secret: 'sssh, quiet! it\'s a secret!',
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 2,
+            sameSite: true,
+            secure: process.env.NODE_ENV === 'production'
+        }
+    })
+)
+
+// index route
+app.use('', indexRouter)
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
+
+module.exports = app
