e99a688fc3f4c603ee9ff3706940c7f6d6d11587
IT18118032 <IT18118032@my.sliit.lk>
2021-09-22 23:39:25 +0530

    Event create

--- backend/services/google-calendar.service.js
+++ backend/services/google-calendar.service.js
@@ -1,7 +1,7 @@
-const { google } = require('googleapis');
+const {google} = require('googleapis');
 
-module.exports.listEvents = function(auth, cb) {
-    const calendar = google.calendar({ version: 'v3', auth });
+module.exports.listEvents = function (auth, cb) {
+    const calendar = google.calendar({version: 'v3', auth});
     calendar.events.list({
         calendarId: 'primary',
         timeMin: (new Date()).toISOString(),
@@ -13,16 +13,53 @@ module.exports.listEvents = function(auth, cb) {
         const events = res.data.items;
         if (events.length) {
             cb(events)
-                //   console.log(events);
-                // console.log('Upcoming 10 events:');
-                // events.map((event, i) => {
-                //     cb({start: event.start.dateTime || event.start.date, event: event.summary})
-                // //   const start = event.start.dateTime || event.start.date;
-                // //   console.log(`${start} - ${event.summary}`);
-                // // cb(items);
-                // });
+            console.log('EVENT COUNT - '+ events.length);
         } else {
             console.log('No upcoming events found.');
         }
+    });
+}
+
+module.exports.createEvent = function (auth, cb) {
+    const eventConstant = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2021-09-29T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2021-09-30T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': false,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    };
+
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.insert({
+        auth: auth,
+        calendarId: 'primary',
+        resource: eventConstant,
+    }, function(err, event) {
+        if (err) {
+            console.log('There was an error contacting the Calendar service: ' + err);
+            return;
+        }
+        console.log('Event created: %s', event);
+        cb(event)
     });
 }
