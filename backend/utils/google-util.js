79faf5aa7179b5da5fbc7e94ef0b485485f464a1
IT18118032 <raveen6666@gmail.com>
2021-10-02 22:08:43 +0530

    Add docs on methods

--- backend/utils/google-util.js
+++ backend/utils/google-util.js
@@ -2,14 +2,20 @@ const { google } = require('googleapis');
 
 require('dotenv').config()
 
-// google app config
+/*
+    Google app configuration
+ */
 const googleConfig = {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     redirect: 'http://localhost:3000/redirect'
 }
 
-// scopes use for the application
+/*
+    Google API scopes for
+        - readonly permission contacts
+        - Edit permission and read permission calendar
+ */
 const defaultScope = [
     'https://www.googleapis.com/auth/calendar',
     'https://www.googleapis.com/auth/contacts.readonly',
@@ -17,7 +23,9 @@ const defaultScope = [
     'email'
 ]
 
-// oauth2 client
+/*
+    Get google client
+ */
 function createConnection() {
     return new google.auth.OAuth2(
         googleConfig.clientId,
@@ -26,7 +34,9 @@ function createConnection() {
     );
 }
 
-// generate authentication url
+/*
+    Generate authentication - obtain a new refresh_token
+ */
 function getConnectionUrl(auth) {
     return auth.generateAuthUrl({
         access_type: 'offline',
@@ -36,14 +46,18 @@ function getConnectionUrl(auth) {
 }
 
 
-// get auth url
+/*
+    get Auth URL
+ */
 module.exports.urlGoogle = function() {
     const auth = createConnection();
     const url = getConnectionUrl(auth);
     return url;
 }
 
-// get oauth2 api
+/*
+    Get Auth API
+ */
 function getOAuth2(auth) {
     return google.oauth2({
         auth: auth,
@@ -51,6 +65,9 @@ function getOAuth2(auth) {
     });
 }
 
+/*
+    Get Google user profile from access token
+ */
 module.exports.getGoogleAccountFromCode = async function(code, cb) {
     const auth = createConnection();
     const { tokens } = await auth.getToken(code);
